ad949caa0fcf866011e8cd13e42530a303776aa4
Sam Christy <schristy@medford.k12.ma.us>
2020-05-16 18:10:12 -0400

    Added the file parse code to the bersam code for curves

--- nc_parser/bersam.py
+++ nc_parser/bersam.py
@@ -1,5 +1,11 @@
+import sys
+import json
+import math
+
 import matplotlib.pyplot as plt
 import numpy as np
+
+filepath = sys.argv[-1]
 
 aX = 0
 aY = 0
@@ -20,49 +26,6 @@ def stepperY(direction):
 	#display.drawPixel(aX, aY, SSD1306_WHITE);
 	#display.display()
 
-def newline(dx, dy):
-
-	error = 0
-	d1 = dx
-	d2 = dy
-	limit = abs(dx)
-
-	sx = 1 
-	sy = 1 
-
-	if dx <= 0: 
-		sx = -1 
-	if dy <= 0: 
-		sy = -1 
-
-	if abs(dx) < abs(dy):
-		limit = abs(dy)
-		d1 = dy
-		d2 = dx
-	
-	for x in range(limit):
-		# Always move either X or Y one step on each pass depending on initial values
-		if abs(dx) >= abs(dy):
-			stepperX(sx)
-		else:
-			stepperY(sy)
-
-		error = abs(error + abs(d2))
-		if (2 * error) >= abs(d1): 
-			error = error - abs(d1)
-
-			# Depending on error move the corresponding direction one step on each pass
-			if abs(dx) >= abs(dy):
-				stepperY(sy)
-			else:
-				stepperX(sx)
-
-def plotLine(x, y): 
-	x = (x - aX)
-	y = (y - aY)
-	newline(x, y)
-
-# note that this is only for counterclockwise
 def arc(x1, y1, x2, y2, r, d):
 	x = x1
 	y = y1
@@ -70,7 +33,7 @@ def arc(x1, y1, x2, y2, r, d):
 	while not complete(x,y,x2,y2):
 		x, y = nextStep(x,y,r,d)
 		points.append((x,y))
-		print('{}, {}'.format(x, y))
+		#print('{}, {}'.format(x, y))
 	return points
 
 def nextStep(x,y,r,d):
@@ -108,45 +71,71 @@ def same_octant(x,y,x2,y2):
 def test_arc():
 	#points = arc(300,0,277,144,300)
 	# start point + offset
-	xd = 165 - 78
-	yd = 81 - 0
-	#points = arc(75 - xd,168 -yd,90 -xd,223 -yd,79,'CW')
-	points = arc(165-xd, 81-yd, 14-xd, 83-yd, 76,'CW')
-	#points = arc(200,0,0,200,200,'CC')
-	x_val = [x[0] for x in points]
-	y_val = [x[1] for x in points]
-	#plt.plot(x_val, y_val, color="red", ls="", marker="o", ms=3)
-	#plt.axis('scaled')
-	#fig, ax = plt.subplots(figsize = (10,10))
-	plt.axis([-300, 300, -300, 300])
-	#ax.scatter(x_val,y_val)
-	plt.plot(x_val, y_val)
-	#print(points)
-	plt.grid(axis='both')
-	plt.show()
+	#xd = 165 - 78
+	#yd = 81 - 0
 
-def test_line():
-	aX = 0
-	aY = 0
-	plotLine(15, 2)
-	x_val = [x[0] for x in test_points]
-	y_val = [x[1] for x in test_points]
+	xd = 276 + 133
+	yd = 212 - 0
+
+	#points = arc(75 - xd,168 -yd,90 -xd,223 -yd,79,'CW')
+	#points = arc(165-xd, 81-yd, 14-xd, 83-yd, 76,'CW')
+	#points = arc(276, 212, 250, 134, 133,'CW')
+	points = arc(200,0,0,200,200,'CC')
+	x_val = [x[0]  for x in points] 
+	y_val = [x[1]  for x in points]
+
 	plt.axis([-300, 300, -300, 300])
-	#plt.set_axisbelow(True)
 	plt.minorticks_on()
-# Customize the major grid
 	plt.grid(which='major', linestyle='solid', linewidth='0.5', color='red')
-# Customize the minor grid
 	plt.grid(which='minor', linestyle=':', linewidth='0.5', color='black')
 	plt.plot(x_val, y_val)
-	#plt.grid(axis='both')
 	plt.show()
-	print(test_points)
+
+def parse_line():
+	with open(filepath, 'r') as fp:
+		gcode = {}
+		points = []
+		points_total = []
+		X0 = 276 
+		Y0 = 212 
+		for line in fp:
+			mylist = line.split()
+			if not len(mylist) == 0:
+				if mylist[0] == 'G1' and 'F' not in mylist[1]:
+					X1 = int(round(float(mylist[1].strip('X'))))
+					Y1 = int(round(float(mylist[2].strip('Y'))))
+					points = plotLine(X0, Y0, X1, Y1) 	
+
+					#print(*points, sep = '\n')
+					print("point")
+					X0 = points[len(points) - 1][0]
+					Y0 = points[len(points) - 1][1]
+					points_total.extend(points)
+					#gcode = json.dumps({'G1':[ float(mylist[1].strip('X')), float(mylist[2].strip('Y'))]})
+					#gcode = json.dumps({'G1':[ int(round(float(mylist[1].strip('X')))), int(round(float(mylist[2].strip('Y'))))]})
+					#print(gcode)
+				elif mylist[0] == 'G2':
+					X1 = int(round(float(mylist[1].strip('X'))))
+					Y1 = int(round(float(mylist[2].strip('Y'))))
+					Xoff = X0 - int(round(float(mylist[3].strip('I'))))
+					Yoff = Y0 - int(round(float(mylist[4].strip('J'))))
 	
+					points = arc(75 - xd,168 -yd,90 -xd,223 -yd,79,'CW')
+	#
+					#opp = float (mylist[3].strip('I'))
+					#adj = float (mylist[4].strip('J'))
+					#radius = math.sqrt(opp**2 + adj**2)
+					#gcode = json.dumps({'G2':[ float(mylist[1].strip('X')), float(mylist[2].strip('Y')), radius]})
+				elif mylist[0] == 'G3':
+					opp = float (mylist[3].strip('I'))
+					adj = float (mylist[4].strip('J'))
+					radius = math.sqrt(opp**2 + adj**2)
+					gcode = json.dumps({'G3':[ float(mylist[1].strip('X')), float(mylist[2].strip('Y')), radius]})
+		print(*points_total, sep = '\n')
+		graph_line(points_total)
 
 def main():
-	test_line()
-	#test_arc()
+	test_arc()
 
 if __name__ == "__main__":
 	main()
